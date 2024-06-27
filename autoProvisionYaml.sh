48ccd74e293ac7949b052df123eeba82f1e9a8a1
Jas Suri <33685132+jasjeetsuri@users.noreply.github.com>
2024-06-27 11:30:29 +0100

    Update autoProvisionYaml.sh

--- autoProvisionYaml.sh
+++ autoProvisionYaml.sh
@@ -5,10 +5,16 @@ REPO_URL="https://github.com/jasjeetsuri/myk3s.git"
 TARGET_DIR="/var/lib/rancher/k3s/server/manifests/homelab"
 
 uninstall_k3s() {
-  /usr/local/bin/k3s-uninstall.sh
+  
+  if command -v k3s &> /dev/null; then
+    echo "K3s found. Uninstalling K3s..."
+    /usr/local/bin/k3s-uninstall.sh
+    echo "K3s uninstalled successfully."
+  else
+    echo "K3s was not found"
+  fi
 }
 
-# Step 0: Install K3s if not already installed
 install_k3s() {
   if ! command -v k3s &> /dev/null; then
     echo "K3s not found. Installing K3s..."
@@ -20,16 +26,15 @@ install_k3s() {
   fi
 }
 
-install_coral_tpu_and_intel_gpu_drivers() {
-  apt install gnupg -y
+install_dependancies() {
+  apt install gnupg  wget curl sudo jq nfs-common git open-iscsi -y
   echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
-  apt install libedgetpu1-std intel-media-va-driver vainfo  -y
+  apt install libedgetpu1-std intel-media-va-driver -y
+  apt update
 }
 
-# Step 1: Install kubeseal if not already installed
 install_kubeseal() {
   if ! command --version kubeseal &> /dev/null; then
-    apt install wget curl sudo jq -y
 
     # Fetch the latest sealed-secrets version using GitHub API
     KUBESEAL_VERSION=$(curl -s https://api.github.com/repos/bitnami-labs/sealed-secrets/tags | jq -r '.[0].name' | cut -c 2-)
@@ -53,9 +58,7 @@ install_kubeseal() {
   fi
 }
 
-# Step 3: Apply secrets for floudflare tunnel
 apply_secrets() {
-  # Variables
   NFS_SERVER="192.168.1.238"
   NFS_SHARE="/k3s"
   MOUNT_POINT="/mnt/nas"
@@ -76,9 +79,6 @@ apply_secrets() {
     sudo mkdir -p "$MOUNT_POINT"
   fi
 
-  # Install NFS client tools
-  apt install nfs-common
-
   # Mount the NFS share
   echo "Mounting NFS share $NFS_SERVER:$NFS_SHARE to $MOUNT_POINT"
   sudo mount -t nfs "$NFS_SERVER:$NFS_SHARE" "$MOUNT_POINT"
@@ -90,38 +90,10 @@ apply_secrets() {
   # Apply the secrets file using kubectl
   echo "Applying $DEST_DIR/$SECRET_FILE using kubectl"
   kubectl apply -f "$DEST_DIR/$SECRET_FILE" -n "$KUBE_NAMESPACE"
-
-  # Unmount the NFS share
-  #echo "Unmounting $MOUNT_POINT"
-  #sudo umount "$MOUNT_POINT"
-}
-
-# Step 3: Install Git if not already installed
-install_git() {
-  if ! command -v git &> /dev/null; then
-    echo "Git not found. Installing Git..."
-    apt update
-    apt install -y git
-    echo "Git installed successfully."
-  else
-    echo "Git is already installed."
-  fi
-}
-
-install_iscsi() {
-  if ! command -v iscsiadm &> /dev/null; then
-    echo "iscsiadm not found. Installing open-iscsi..."
-    apt update
-    apt install -y open-iscsi
-    echo "open-iscsi installed successfully."
-  else
-    echo "open-iscsi is already installed."
-  fi
 }
 
 
-# Step 4: Clone the repository if it doesn't exist, or pull the latest changes if it does
-update_repo() {
+clone_repo() {
   if [ ! -d "$TARGET_DIR" ]; then
     echo "Directory $TARGET_DIR does not exist. Creating it..."
     mkdir -p "$TARGET_DIR"
@@ -147,22 +119,16 @@ uninstall_k3s
 # Install K3s if not already installed
 install_k3s
 
+# Install dependancies
+install_dependancies
+
 # Install kubeseal if not already installed
 install_kubeseal
 
 # Apply secrets
 apply_secrets
 
-# Install Git if not found
-install_git
-
-# Install iscsi tools if not found
-install_iscsi
-
-# Install coral TPU drivers
-install_coral_tpu_and_intel_gpu_drivers
-
 # Clone or pull the repository
-update_repo
+clone_repo
 
 echo "Setup, repository, update, completed."
